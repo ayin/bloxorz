4fed937a601bc0ffbf69e06aa843ebb9768317f3
unknown <ayin1989dy@qq.com>
2012-08-26 15:26:54 +0800

    gfan

--- src/com/androidapk/ayin/bloxorz/MyAd.java
+++ src/com/androidapk/ayin/bloxorz/MyAd.java
@@ -1,10 +1,5 @@
 package com.androidapk.ayin.bloxorz;
 
-
-import com.adview.AdViewLayout;
-import com.adview.AdViewTargeting;
-import com.adview.AdViewTargeting.RunMode;
-import com.adview.AdViewTargeting.UpdateMode;
 import com.google.ads.AdRequest;
 import com.google.ads.AdSize;
 import com.google.ads.AdView;
@@ -21,22 +16,22 @@ public class MyAd{
 		this.mContext = context;
 		LinearLayout layout = (LinearLayout)((Activity)mContext).findViewById(R.id.adLayout); 
 		
-	/*	AdView adView = new AdView((Activity)mContext, AdSize.BANNER, "a1502352a097930");
+		AdView adView = new AdView((Activity)mContext, AdSize.BANNER, "a1502352a097930");
         layout.addView(adView);
         // Initiate a generic request to load it with an ad
         AdRequest request = new AdRequest();
         adView.loadAd(request);
        // request.setTesting(true);
-    */    
+        
         /* 下面两行仅仅用于调试，发布前注释掉 */ 
 		//AdViewTargeting.setRunMode(RunMode.TEST); 
 		//AdViewTargeting.setUpdateMode(UpdateMode.EVERYTIME); 
-		AdViewLayout adViewLayout = new AdViewLayout((Activity)mContext, "SDK2012130901080371ml3ixqsah7fv8");
+//		AdViewLayout adViewLayout = new AdViewLayout((Activity)mContext, "SDK2012130901080371ml3ixqsah7fv8");
 		
 		//adViewLayout.setAdViewInterface(this); 
 		//if(adViewLayout.getHeight()==0){layout.setVisibility(View.GONE);return;}
 		
-		layout.addView(adViewLayout); 
-		layout.invalidate(); 
+	//	layout.addView(adViewLayout); 
+	//	layout.invalidate(); 
 	}
 }
